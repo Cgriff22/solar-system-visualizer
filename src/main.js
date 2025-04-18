c1ce4e6626d1f5d495a83090cdfcae2df5110a11
Carter Griffin <cartergriffin22@gmail.com>
2025-04-18 02:55:19 -0400

    adding multi date functionality

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const dateStr = currentDate.toISOString().split("T")[0];
     console.log("Fetching date:", dateStr);
     // fetch and store skyfield data
-    fetch("https://solar-system-backend.onrender.com/positions?date=${iso}")
+    fetch("https://solar-system-backend.onrender.com/positions?date=${dateStr}")
       .then(res => res.json())
       .then(data => { 
         planetPositions = data;
